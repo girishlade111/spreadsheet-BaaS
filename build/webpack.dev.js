331491f5fe3ba08632e6316238e60563b8f93433
Girish Lade <girishlade111@gmail.com>
2026-05-07 02:22:09 -0700

    feat: add webpack development configuration with dev server and plugin support

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -26,6 +26,7 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     host: 'localhost',
-    contentBase: '../dist',
+    port: 8080,
+    contentBase: './dist',
   },
 });
